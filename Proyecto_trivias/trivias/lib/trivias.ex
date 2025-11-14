d050f50420f021addfedabadc261a30cd8bd0a96
harold2rendon <142748727+harold2rendon@users.noreply.github.com>
2025-11-14 14:58:26 -0500

    Add files via upload

---
+++ Proyecto_trivias/trivias/lib/trivias.ex
@@ -0,0 +1,18 @@
+defmodule Trivias do
+  @moduledoc """
+  Documentation for `Trivias`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Trivias.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
